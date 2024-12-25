7d4e47ec286df986ab5b9a6ff98a8846f1981683
Bharat-Dua <bharatdua13@gmail.com>
2024-12-25 20:15:20 +0530

    javascript practice

--- 1.interview.js
+++ 1.interview.js
@@ -1,16 +1,21 @@
 //* 1.
 console.log(1 + "1"); // "11" (string concatenation)
+
 //* 2.
 console.log(1 * "1"); // 1 (number multiplication) , string gets converted to number
+
 //* 3.
 console.log(1 - "1"); // 0 (number subtraction) , string gets converted to number
+
 //* 4.
 console.log("1" - 1); // 0 (number subtraction) , string gets converted to number
+
 //* 5.
 setTimeout(function () {
   console.log(10); // 10 after 10 ms
 }, 10);
 console.log(2); // 2 (immediately executed)
+
 //* 6.
 console.log("1" - 1);
 setTimeout(function () {
@@ -23,3 +28,18 @@ setTimeout(() => {
 setTimeout(() => {
   console.log(12);
 }, 9);
+
+//* 7.
+a = 10;
+console.log(a); // 10
+var a;
+
+//* 8.
+a = 10;
+console.log(a); // Reference error
+let a;
+
+//*9.
+console.log(a); // undefined
+a = 10;
+var a;
